73bdd90eeef018c8dc0129c790cd08b156024530
Nikita Klimovich <nick.klimovich@gmail.com>
2013-04-01 20:39:18 -0500

    completed Piano.c

---
+++ Lab7/Piano.c
@@ -0,0 +1,26 @@
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "SysTickInts.h"
+#include "lm3s1968.h"
+#include "DAC.h"
+
+void Piano_Init(void){
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOG;
+	
+	GPIO_PORTG_DIR_R = 0x00;
+	GPIO_PORTF_DEN_R |= 0x0F;
+	GPIO_PORTF_AFSEL_R |= 0x00;
+}
+
+void Piano_In (void) {
+	if (GPIO_PORTF_DATA_R == 0x01) {
+		Sound_Play;
+	} 
+ 	
+	
+	
+	
+	
+	
+	
+}
