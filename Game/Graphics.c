33fa9f180dd5c051768d9d5fdfb2829446302e8f
Nikita Klimovich <nick.klimovich@gmail.com>
2013-04-23 21:27:33 -0500

    rotation coded

--- Game/Graphics.c
+++ Game/Graphics.c
@@ -2,8 +2,13 @@
 #include "drivers/rit128x96x4.h"
 #include "inc/lm3s1968.h"
 #include "graphics.h"
+#include <math.h>
 
 unsigned char g_frame[6144];
+typedef struct { 
+	char x,y;
+} Point;
+
 
 void ClearScreen(void) {
 	int i;
@@ -100,6 +105,38 @@ void DrawImageFast(unsigned char* data, unsigned int x,
 	for (j = 0; j < height; j++) {
 		for (i = 0; i < width/2; i++) {
 			g_frame[(j+y)*64+x/2+i] = data[j*width/2+i];
+		}
+	}
+}
+
+
+
+
+void RotateImage(unsigned char* data, unsigned int x, 
+								unsigned int y, unsigned int width, 
+								unsigned int height, unsigned int angle, unsigned int scale)
+{
+	int i,j; 
+	int newI, newJ, newI2, newJ2;
+	int newM;
+	int centerX = width/2;
+	int centerY = height/2;
+	int mat11 = 128.0*scale*cos(angle*180/3.14);
+	int mat12 = -128.0*scale*sin(angle*180/3.14);
+	int mat21 = 128.0*scale*sin(angle*180/3.14);
+	int mat22 = 128.0*scale*cos(angle*180/3.14);
+	for (j = 0; j < height; j++) {
+		for (i = 0; i < width/2; i++) {
+			
+			newI = centerX + (mat11*(i*2 - centerX) + mat12*(j - centerY))/128 + 0.5;
+			newI2 = centerX + (mat11*(i*2 + 1 - centerX) + mat12*(j - centerY))/128 + 0.5;
+			newJ = centerY + (mat21*(i*2 - centerX) + mat22*(j - centerY))/128 + 0.5;
+			newJ2 = centerY + (mat21*(i*2 + 1 - centerX) + mat22*(j - centerY))/128 + 0.5;
+			
+			newM = (data[j*width/2+i]&0xF0)/16;
+			SetPixel(newI + x, newJ + y, newM);
+			newM = (data[j*width/2+i]&0x0F);
+			SetPixel(newI2 + x, newJ2 + y, newM);
 		}
 	}
 }
