a3fd4469b748549e51fc985ea68cb01a902d7f2c
Anthony Weems <amlweems@gmail.com>
2013-05-01 16:57:38 -0500

    rising edge

--- Game/Input.c
+++ Game/Input.c
@@ -7,12 +7,12 @@
 
 void InputInit(void){
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOD;
-  NOP();
+  NOP();	
   GPIO_PORTD_DIR_R &= ~0x0F;// make PG3-7 in (PC3-7 button)
   GPIO_PORTD_DEN_R |= 0x0F; // enable digital I/O on PC7
   GPIO_PORTD_IS_R &= ~0x0F; // PC7 is edge-sensitive (default setting)
   GPIO_PORTD_IBE_R &= ~0x0F;// PC7 is not both edges (default setting)
-  GPIO_PORTD_IEV_R |= ~0x0F; // PC7 falling edge event
+  GPIO_PORTD_IEV_R |= 0x0F; // PC7 rising edge event
   GPIO_PORTD_ICR_R = 0x0F;  // clear flag7
   GPIO_PORTD_IM_R |= 0x0F;  // enable interrupt on PC7
 	NVIC_PRI0_R = (NVIC_PRI0_R&0x0FFFFFFF)|0x20000000; // bits 29-31
